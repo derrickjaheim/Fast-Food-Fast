ff63f21a6524d7c620ae0f3677ecb55699b5d7c6
derrickjaheim <ledro187@gmail.com>
2018-09-11 00:20:03 +0300

    New changes added

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,3 +1,17 @@
-document.getElementById("upvote").addEventListener("click", function(event){
-	alert("You've upvoted an answer");
-}, false);
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+</script>
+
