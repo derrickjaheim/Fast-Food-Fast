ee90f42d15e665c15fe52a23f54b6f909117d57f
derrickjaheim <ledro187@gmail.com>
2018-09-25 10:50:00 +0300

    Changes made to the Files in UI

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,3 +1,29 @@
-document.getElementById("upvote").addEventListener("click", function(event){
-	alert("You've upvoted an answer");
-}, false);
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+function Authent(){
+    var username = document.getElementById("field1");
+    var password = document.getElementById("field2");
+    if(username.value ==='user' && password.value ==="user"){
+              window.location = "post.html"
+    }
+   else if(username.value ==='admin' && password.value ==="admin"){
+              window.location = "adminpost.html"
+    }
+    else {
+        alert("Wrong credentials")
+    }
+  }
+
