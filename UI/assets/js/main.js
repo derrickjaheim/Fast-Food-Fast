203675bcc29b877bcc4f1910ac9756ecec66a52e
derrickjaheim <43031429+derrickjaheim@users.noreply.github.com>
2018-09-27 16:10:01 +0300

    Update main.js

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -28,20 +28,4 @@ function Authent(){
     }
   }
 
-<script>
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
-</script>
 
